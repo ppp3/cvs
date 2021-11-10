1ef6d4282262bfe8412201cb860b4d543e0dc2fc
Your Name <you@example.com>
2021-11-10 13:22:21 +0100

    fdgfd

--- tests/cypress/integration/4.spec.js
+++ tests/cypress/integration/4.spec.js
@@ -2,13 +2,13 @@
 
 context('Window', () => {
   beforeEach(() => {
-    cy.visit('http://www.gmx.de')
+    cy.visit('http://www.gmx.net')
 	cy.fixture('example').as('exampleJson')
   })
 
 
   it('cy.title() - get the title', () => {
     // https://on.cypress.io/title
-    cy.title().should('include', 'Anmeldung')
+    cy.title().should('include', 'GMX')
   })
 })
